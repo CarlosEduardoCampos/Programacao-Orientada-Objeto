5d32f977496384afb4165b83b5209e161fd40c85
Carlos <Carlos.admcode@gmail.com>
2021-07-05 13:45:15 -0300

    Implementação da Atividade - cria frame com checkbox

---
+++ aula_05/src/Atividade/CheckBox_Frame.java
@@ -0,0 +1,66 @@
+package Atividade;
+
+import java.awt.Checkbox;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+
+public class CheckBox_Frame extends JFrame 
+{
+	
+	private JTextField textField;
+	private JCheckBox  boldJCheckBox;
+	private JCheckBox  italicJCheckBox;
+	
+	//Costrutor da classe
+	public CheckBox_Frame()
+	{
+		super("CheckBox teste");//Titulo do frame
+		setLayout(new FlowLayout());
+		//
+		textField = new JTextField(" Escolha um estilo de fonte ", 25);//configura aparencia do frame
+		textField.setFont(new Font("Serif", Font.PLAIN, 14));//configura aparencia da font do frame
+		add(textField);// adciona textField ao frame
+		//
+		boldJCheckBox = new JCheckBox("Negrito");//cria caixa de seleção para negrito
+		italicJCheckBox =new JCheckBox("Italico");//cria caixa de seleçãopara itálico
+		add(boldJCheckBox);
+		add(italicJCheckBox);
+		
+		//listeners registradores para JCheckBoxes
+		Checkbox handler = new Checkbox();
+		boldJCheckBox.addItemListener( (ItemListener) handler);
+		
+		//Classe interna private para tratamento de evento
+		class Checkbox implements ItemListener
+		{
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				Font font = null;//armazena anova font
+				
+				//determina que CheckBoxes estão marcados e cria o font
+				if(boldJCheckBox.isSelected() && italicJCheckBox.isSelected())
+				{
+					font = new Font("Serif", Font.BOLD + Font.ITALIC, 14);
+				}
+				else if(boldJCheckBox.isSelected())
+				{
+					font = new Font("Serif", Font.BOLD, 14);
+				}
+				else if(italicJCheckBox.isSelected())
+				{
+					font = new Font("Serif", Font.ITALIC, 14);
+				}
+				else {
+					font = new Font("Serif", Font.PLAIN, 14);
+				}
+				//
+				textField.setFont(font);//mostra a fonte configurada
+			}//fim método itemStateCharged
+		}//fim da class CheckBox
+	}//fim da classe CheckBox_Frame
+}
---
+++ aula_05/src/Atividade/CheckBox_Text.java
@@ -0,0 +1,15 @@
+package Atividade;
+
+import javax.swing.JFrame;
+
+public class CheckBox_Text
+{
+	public static void main(String[] args)
+	{
+		CheckBox_Frame checkBoxFrame = new CheckBox_Frame();
+		//
+		checkBoxFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		checkBoxFrame.setSize( 275, 100 ); // configura o tamanho do frame
+		checkBoxFrame.setVisible( true ); // exibe o frame
+	}
+}
